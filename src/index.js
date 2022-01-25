6ccab9699dfa3b65d1453dd7b270474d3b1b2959
Felipe Lisbão <flisbao@ciandt.com>
2022-01-25 14:23:31 -0300

    adding configuration prompt

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 #!/usr/bin/env node
-require("dotenv").config();
-const config = require("./config/config");
-const api = require("./api/api");
-const server = require("./server/server");
-const sessionService = require("./api/services/session-service");
-const logger = require('./utils/light-logger');
+const args = process.argv.slice(2);
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-process.on("SIGINT", () => process.exit(0));
-
-(async () => {
-  try {
-    await api.init();
-    await server.init();
-
-    if (config.defaultSession) {
-      await sessionService.activateSession(config.defaultSession);
-    }
-  } catch (error) {
-    logger.error(error);
-    process.exit(0);
-  }
-})();
+if (args[0] === 'init') {
+  (async () => {
+    const { configureApi } = require('./configureApi')
+    await configureApi();
+  })();
+} else {
+  (async () => {
+    const { initApi } = require('./initializeApi')
+    await initApi()
+  })();
+}
---
+++ src/initializeApi.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const config = require("./config/config");
+const api = require("./api/api");
+const server = require("./server/server");
+const sessionService = require("./api/services/session-service");
+const logger = require('./utils/light-logger');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.on("SIGINT", () => process.exit(0));
+
+const initApi = async () => {
+    try {
+        await api.init();
+        await server.init();
+
+        if (config.defaultSession) {
+            await sessionService.activateSession(config.defaultSession);
+        }
+    } catch (error) {
+        logger.error(error);
+        process.exit(0);
+    }
+}
+
+module.exports = { initApi }
